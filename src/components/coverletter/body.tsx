ac29cb28bef0b9626e68a449a73dc1ab690bb22d
pdev6373 <pdev6373@gmail.com>
2022-05-06 04:19:33 +0100

    made coverletter section responsive

--- src/components/coverletter/body.tsx
+++ src/components/coverletter/body.tsx
@@ -13,13 +13,17 @@ export const Body = () => {
           Be the first to know about our products and services by subscribing to
           our newsletter
         </p>
-        <form className="coverletter__content__body__form">
+        <form
+          className="coverletter__content__body__form"
+          onSubmit={(e) => e.preventDefault()}
+        >
           <input
             className="coverletter__content__body__form__email"
             type="text"
             placeholder="email address"
           />
           <input
+            onClick={(e) => e.preventDefault()}
             className="coverletter__content__body__form__submit"
             type="submit"
             value="Subscribe"
